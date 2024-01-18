5b836fb2b6d335d02df486af89b24b16533c4282
Dmytro Karpenko <kardmitriy@gmail.com>
2024-01-19 00:31:53 +0200

    Header finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        666: '#666666',
+        bgBTN: '#222',
+      },
+
+      boxShadow: {
+        l: '0px 3px 15px 0px rgba(0, 0, 0, 0.15);',
+      },
+
+      lineHeight: {
+        110: '110%',
+      },
+    },
   },
   plugins: [],
 };
